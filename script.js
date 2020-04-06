821eff33e3322ce9514fbfc048a156f5f8a9b5bb
Hector Gonzalez <hectarek13@gmail.com>
2020-04-06 17:07:37 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+var addButton = document.getElementById('addButton');
+
+var list = document.getElementById('todoList');
+
+addButton.addEventListener('click', ()=>{
+
+    var todoList = document.querySelector('#todoList')
+
+    console.log(todoList)
+
+    var newTodoItem = document.createElement('li')
+    newTodoItem.classList.add('list-group-item')
+    newTodoItem.classList.add('p-3')
+    newTodoItem.classList.add('hvr-back-pulse')
+
+
+    var userInput = document.getElementById('userInput').value;
+
+    newTodoItem.innerHTML = ('<img class="smallCheck mx-2" src="https://cdn2.iconfinder.com/data/icons/flat-ui-icons-24-px/24/checkmark-24-512.png" alt="checkmark"> ' + ' <span>' + userInput + '</span>') 
+
+    todoList.appendChild(newTodoItem)
+
+    document.getElementById('userInput').value = ''
+})
+
+list.addEventListener('click', (e)=>{
+
+    
+    e.remove();
+        
+})
